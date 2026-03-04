0fcd92a57b2e9ff3747fcdba613c19a3e4b422dc
Dosh Barzanji <Doshbarzanji@gmail.com>
2026-03-04 09:24:31 -0700

    Completed task lister form submission feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.querySelector("#create-task-form");
+const input = document.querySelector("#new-task-description");
+const tasksList = document.querySelector("#tasks");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const taskText = input.value;
+
+  if (taskText.trim() === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  tasksList.appendChild(li);
+
+  input.value = "";
 });
